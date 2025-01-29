e8e5769598d5c4003728ba9593259134780f516f
RafaelPil <rafik.pilumyan@gmail.com>
2025-01-29 20:02:54 +0200

    Update: UI server side, useActionState

--- shop-ui/app/auth/signup/page.tsx
+++ shop-ui/app/auth/signup/page.tsx
@@ -1,18 +1,26 @@
-import { Button, Link, Stack, TextField } from '@mui/material'
-import React from 'react'
+"use client";
+import { Button, Link, Stack, TextField } from "@mui/material";
+import React, { useActionState } from "react";
 import NextLink from "next/link";
+import createUser from "./create-user";
 
 const Signup = () => {
-  return (
-    <Stack spacing={2} className="w-full max-w-xs">
-      <TextField label="Email" variant="outlined" type="email" />
-      <TextField label="Password" variant="outlined" type="password" />
-      <Button variant="contained">Signup</Button>
-      <Link component={NextLink} href="/auth/login" className="self-center">
-        Login
-      </Link>
-    </Stack>
-  )
-}
+  const [state, formAction] = useActionState(createUser, { error: "" });
 
-export default Signup
+  return (
+    <form action={formAction} className="w-full max-w-xs">
+      <Stack spacing={2}>
+        <TextField label="Email" variant="outlined" type="email" />
+        <TextField label="Password" variant="outlined" type="password" />
+        <Button type="submit" variant="contained">
+          Signup
+        </Button>
+        <Link component={NextLink} href="/auth/login" className="self-center">
+          Login
+        </Link>
+      </Stack>
+    </form>
+  );
+};
+
+export default Signup;
