da01afede5fdd433947bf3028411950dac59521a
RafaelPil <rafik.pilumyan@gmail.com>
2025-02-13 19:30:38 +0200

    Update: JSON Payloads

--- shop-ui/app/auth/signup/create-user.ts
+++ shop-ui/app/auth/signup/create-user.ts
@@ -2,27 +2,13 @@
 
 import { API_URL } from "@/app/constants/api";
 import { getErrorMessage } from "@/app/util/errors";
+import { post } from "@/app/util/fetch";
 import { redirect } from "next/navigation";
 
 export default async function createUser(_prevState: any, formData: FormData) {
-  const userData = {
-    email: formData.get("email"),
-    password: formData.get("password"),
-  };
-
-  const res = await fetch(`${API_URL}/users`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(userData),
-  });
-
-  const parsedRes = await res.json();
-  if (!res.ok) {
-    // console.log(parsedRes);
-    return { error: getErrorMessage(parsedRes) };
+  const { error } = await post("users", formData);
+  if (error) {
+    return { error };
   }
-
-  redirect("/");
+  redirect('/')
 }
