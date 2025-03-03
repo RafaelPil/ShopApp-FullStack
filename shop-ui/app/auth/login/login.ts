e05e1b5c3c71f60027a7711b1b451849eda47cb8
RafaelPil <rafik.pilumyan@gmail.com>
2025-03-03 19:55:39 +0200

    Update: Server Cookies

--- shop-ui/app/auth/login/login.ts
+++ shop-ui/app/auth/login/login.ts
@@ -3,6 +3,9 @@
 import { formError } from "@/app/common/form-error.interface";
 import { API_URL } from "@/app/constants/api";
 import { getErrorMessage } from "@/app/util/errors";
+import { cookies } from "next/headers";
+import { jwtDecode } from "jwt-decode";
+
 import { redirect } from "next/navigation";
 
 export default async function login(_prevState: formError, formData: FormData) {
@@ -20,5 +23,20 @@ export default async function login(_prevState: formError, formData: FormData) {
     return { error: getErrorMessage(parsedRes) };
   }
 
+  setAuthCookie(res);
   redirect("/");
 }
+
+const setAuthCookie = async (response: Response) => {
+  const setCookieHeader = response.headers.get("Set-Cookie");
+  if (setCookieHeader) {
+    const token = setCookieHeader.split(";")[0].split("=")[1];
+    (await cookies()).set({
+      name: "Authentication",
+      value: token,
+      secure: true,
+      httpOnly: true,
+      expires: new Date(jwtDecode(token).exp! * 1000),
+    });
+  }
+};
